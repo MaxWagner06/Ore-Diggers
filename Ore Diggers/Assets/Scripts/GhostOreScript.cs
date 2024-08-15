e3b5c357c238d6016d59d65107bc958b1094b091
CanMuhammetAliCin <canmuhammetalicin1@gmail.com>
2024-08-16 02:08:48 +0300

    I am bad

--- Ore Diggers/Assets/Scripts/GhostOreScript.cs
+++ Ore Diggers/Assets/Scripts/GhostOreScript.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class GhostOreScript : MonoBehaviour
 {
-    public bool canOreSpawn;
+    public bool canSpawn = true;
+    public Rigidbody2D rb;
 
     private void Start()
     {
-        transform.position += Vector3.left * 3f;
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-5, 0);
+        StartCoroutine(Wait());
+
+        IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(1f);
+            rb.velocity = new Vector2(0, 0);
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        canOreSpawn = true;
+        if(col.gameObject.tag == "Ore")
+        {
+            canSpawn = false;
+        }
     }
+
+
 }
--- Ore Diggers/Assets/Scripts/RandomOreSpawn.cs
+++ Ore Diggers/Assets/Scripts/RandomOreSpawn.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class RandomOreSpawn : MonoBehaviour
 {
-    public List<Vector2> orePosList = new List<Vector2>();
-
     public GameObject smallStoneOre, bigStoneOre, coalOre, copperOre, ironOre, silverOre, goldOre,
                       platinumOre, iridiumOre, uraniumOre,ghostOre;
     public int smallStoneOreCount, bigStoneOreCount, coalOreCount, copperOreCount, ironOreCount, silverOreCount,
                goldOreCount, platinumOreCount, iridiumOreCount, uraniumOreCount;
 
     public int mapSize;
-    private int randomX, randomY, randomXMultiple, randomYMultiple;
-    public Vector2 randomCoord;
+    private int randomX, randomY;
+    private Vector2 randomCoord, ghostSpawnCoord;
+
+    GhostOreScript ghostOreObject;
+
+
 
     private void Start()
     {
@@ -21,7 +23,25 @@ public class RandomOreSpawn : MonoBehaviour
     }
 
     public void StartOreSpawn()
-    {  
+    {
+
+        for (int i = 0; i < smallStoneOreCount; i++)
+        {
+            randomX = Random.Range(-mapSize, mapSize);
+            randomY = Random.Range(-mapSize, mapSize);
+            randomCoord = new Vector2(randomX, randomY);
+            ghostSpawnCoord = new Vector2(randomX + 4.5f, randomY);
+            ghostOreObject = Instantiate(ghostOre, ghostSpawnCoord, Quaternion.identity).GetComponent<GhostOreScript>();
+            if (ghostOreObject.canSpawn)
+            {
+                Destroy(ghostOreObject.gameObject);
+                Instantiate(smallStoneOre, randomCoord, Quaternion.identity);
+            }
+
+
+        }
+
+
     }
 
 }
