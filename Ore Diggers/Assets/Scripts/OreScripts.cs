672e6f6740f1c3259d0d1eaec56d374e2a1a6a7e
MaxWagner06 <ali1889eren@gmail.com>
2024-08-16 23:45:01 +0300

    Revert "Sex Update"
    This reverts commit c17da1b4dc495eac94c29c68917692f2d2b38ca2.

--- Ore Diggers/Assets/Scripts/OreScripts.cs
+++ Ore Diggers/Assets/Scripts/OreScripts.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class OreScripts : MonoBehaviour
 {
     public string oreName;
-    public int oreIndex;
     public int oreDurability;
     public int oreRequiredPickaxeIndex;
     public int oreDropCount;
