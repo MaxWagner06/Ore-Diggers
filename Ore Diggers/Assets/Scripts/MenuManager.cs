a8039547ee23ccefb40c92ee713b8f26d13de5cd
MaxWagner06 <ali1889eren@gmail.com>
2024-08-17 00:27:33 +0300

    Pause menu and ınventory added(2)

--- Ore Diggers/Assets/Scripts/MenuManager.cs
+++ Ore Diggers/Assets/Scripts/MenuManager.cs
@@ -10,6 +10,7 @@ public class MenuManager : MonoBehaviour
 
     public void Play(){
         SceneManager.LoadScene(1);
+        Time.timeScale = 1;
     }
     public void Options(){
         optionpanel.SetActive(true);
