c17da1b4dc495eac94c29c68917692f2d2b38ca2
CanMuhammetAliCin <canmuhammetalicin1@gmail.com>
2024-08-16 23:23:16 +0300

    Sex Update

--- Ore Diggers/Assets/Scripts/OreScripts.cs
+++ Ore Diggers/Assets/Scripts/OreScripts.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OreScripts : MonoBehaviour
 {
     public string oreName;
+    public int oreIndex;
     public int oreDurability;
     public int oreRequiredPickaxeIndex;
     public int oreDropCount;
