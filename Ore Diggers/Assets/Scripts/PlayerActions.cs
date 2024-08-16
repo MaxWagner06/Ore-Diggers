f0d98ea0dc9bf0dabc7901ba1c98953363d8df91
MaxWagner06 <ali1889eren@gmail.com>
2024-08-16 23:47:19 +0300

    Reapply "Sex Update"
    This reverts commit 672e6f6740f1c3259d0d1eaec56d374e2a1a6a7e.

---
+++ Ore Diggers/Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using TMPro;
+
+public class PlayerActions : MonoBehaviour
+{
+    public int currentPickaxeIndex;
+    [HideInInspector] public int currentDamage;
+    public int handDamage, stonePickaxeDamage, ironPickaxeDamage, goldPickaxeDamage, iridiumPickaxeDamage, MATKAPdamage;
+
+    public GameObject notificationTextObject;
+    public GameObject notificationPanel;
+
+    private void Update()
+    {
+        if(currentPickaxeIndex == 0)
+        {
+            currentDamage = handDamage;
+        }
+        else if(currentPickaxeIndex == 1)
+        {
+            currentDamage = stonePickaxeDamage;
+        }
+        else if (currentPickaxeIndex == 2)
+        {
+            currentDamage = iridiumPickaxeDamage;
+        }
+        else if (currentPickaxeIndex == 3)
+        {
+            currentDamage = goldPickaxeDamage;
+        }
+        else if (currentPickaxeIndex == 4)
+        {
+            currentDamage = iridiumPickaxeDamage;
+        }
+        else if (currentPickaxeIndex == 5)
+        {
+            currentDamage = MATKAPdamage;
+        }
+
+
+        //LeftMouseButton
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.zero);
+            if (hit.collider)
+            {
+                if (hit.collider.gameObject.CompareTag("Ore"))
+                {
+                    OreScripts oreScript = hit.collider.gameObject.GetComponent<OreScripts>();
+                    if (currentPickaxeIndex >= oreScript.oreRequiredPickaxeIndex)
+                    {
+                        oreScript.oreDurability -= currentDamage;
+
+                        if(oreScript.oreDurability <= 0)
+                        {
+                            //Inventory.AddOre(oreScript.oreIndex, oreScript.oreDropCount);
+                            Destroy(hit.collider.gameObject);
+
+                            TextMeshProUGUI notificationText = Instantiate(notificationTextObject, notificationPanel.transform.position, Quaternion.identity).GetComponent<TextMeshProUGUI>();
+                            notificationText.text = "+ " + oreScript.oreDropCount + " " + oreScript.oreName;
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
